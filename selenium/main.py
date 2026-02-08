fcdbd5648d395879dad1fba8d5d0c0bf0a23c6bd
VitorMiguel34 <miguelvictor035@gmail.com>
2026-02-08 20:06:51 -0300

    selenium: exercicio 1

--- selenium/main.py
+++ selenium/main.py
@@ -5,38 +5,42 @@ from selenium.webdriver.common.by import By
 #from selenium.webdriver.support import expected_conditions as EC
 from typing import Any
 
-browser= webdriver.Chrome()
-browser.get("http://localhost:8000")
+def main():
+    browser= webdriver.Chrome()
+    browser.get("http://localhost:8000")
 
-def enviar_input_por_id(id_input: str, valor: Any) -> None:
-    input = browser.find_element(By.ID, id_input)
-    input.send_keys(valor)
+    def enviar_input_por_id(id_input: str, valor: Any) -> None:
+        input = browser.find_element(By.ID, id_input)
+        input.send_keys(valor)
 
-def clicar_em_botao_por_id(id_botao: str) -> None:
-    botao = browser.find_element(By.ID, id_botao)
-    botao.click()
+    def clicar_em_botao_por_id(id_botao: str) -> None:
+        botao = browser.find_element(By.ID, id_botao)
+        botao.click()
 
-def buscar_texto_por_id(id_elemento: str) -> str:
-    elemento = browser.find_element(By.ID, id_elemento)
-    return elemento.text
+    def buscar_texto_por_id(id_elemento: str) -> str:
+        elemento = browser.find_element(By.ID, id_elemento)
+        return elemento.text
 
-valores = {
-    "inputNome": "Joao",
-    "inputIdade": 16
-}
+    valores = {
+        "inputNome": "Joao",
+        "inputIdade": 16
+    }
 
-for id,valor in valores.items():
-    enviar_input_por_id(id,valor)
+    for id,valor in valores.items():
+        enviar_input_por_id(id,valor)
 
-clicar_em_botao_por_id("enviarDados")
+    clicar_em_botao_por_id("enviarDados")
 
-texto = buscar_texto_por_id("resultadoLogin")
-print(texto)
+    texto = buscar_texto_por_id("resultadoLogin")
+    print(texto)
 
-if texto == "Sucesso":
-    print("Login efetuado com sucesso")
-else:
-    print("Falha no login")
+    if texto == "Sucesso":
+        print("Login efetuado com sucesso")
+    else:
+        print("Falha no login")
 
-browser.save_screenshot("site.png")
-browser.quit()
+    browser.save_screenshot("site.png")
+    browser.quit()
+
+if __name__ == "__main__":
+    main()
