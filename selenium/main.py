5293cecbe6ee0f7aa6d4d9b7d14a3ca0ba8a8676
VitorMiguel34 <miguelvictor035@gmail.com>
2026-02-08 19:44:13 -0300

    selenium: automatizando a web

---
+++ selenium/main.py
@@ -0,0 +1,42 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+#from selenium.webdriver.common.keys import Keys
+#from selenium.webdriver.support.wait import WebDriverWait
+#from selenium.webdriver.support import expected_conditions as EC
+from typing import Any
+
+browser= webdriver.Chrome()
+browser.get("http://localhost:8000")
+
+def enviar_input_por_id(id_input: str, valor: Any) -> None:
+    input = browser.find_element(By.ID, id_input)
+    input.send_keys(valor)
+
+def clicar_em_botao_por_id(id_botao: str) -> None:
+    botao = browser.find_element(By.ID, id_botao)
+    botao.click()
+
+def buscar_texto_por_id(id_elemento: str) -> str:
+    elemento = browser.find_element(By.ID, id_elemento)
+    return elemento.text
+
+valores = {
+    "inputNome": "Joao",
+    "inputIdade": 16
+}
+
+for id,valor in valores.items():
+    enviar_input_por_id(id,valor)
+
+clicar_em_botao_por_id("enviarDados")
+
+texto = buscar_texto_por_id("resultadoLogin")
+print(texto)
+
+if texto == "Sucesso":
+    print("Login efetuado com sucesso")
+else:
+    print("Falha no login")
+
+browser.save_screenshot("site.png")
+browser.quit()
