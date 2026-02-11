b30024ce43fe75c7b7eb04326186547c8d5f7e90
VitorMiguel34 <miguelvictor035@gmail.com>
2026-02-11 19:04:09 -0300

    feat: add abertura de pagina com playwright

--- Playwright/main.py
+++ Playwright/main.py
@@ -1,10 +1,12 @@
 from playwright.sync_api import sync_playwright
+import time
 
+url = "http://localhost:8000"
 with sync_playwright() as p:
     browser = p.chromium.launch(headless=False)
     page = browser.new_page()
 
-    page.goto("https://example.com")
+    page.goto(url)
     print(page.title())
-    input()
+    time.sleep(3)
     browser.close()
